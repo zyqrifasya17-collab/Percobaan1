6edf18467ce768226cb073d064ee5e709241a43d
zyqri <zyqrifasya17@gmail.com>
2026-05-20 20:35:50 +0700

    update belajar.js dan test.js"

---
+++ Belajar JS/belajar.js
@@ -0,0 +1,4 @@
+console.log('Percobaan Pertama');
+
+const add = (a, b) => a + b;
+console.log(add(1, 2));
---
+++ Belajar JS/test.js
@@ -0,0 +1,3 @@
+console.log('Percobaan 1.1');
+const add = (a, b) => a + b;
+console.log(add(1, 2));
