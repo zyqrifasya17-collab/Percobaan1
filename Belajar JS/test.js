53c46a7901218794514bd2f4946833768b6bca14
zyqri <zyqrifasya17@gmail.com>
2026-05-22 05:39:05 +0700

    Merevisi commit sebelumnya

--- Belajar JS/test.js
+++ Belajar JS/test.js
@@ -1,8 +1,8 @@
 const modulus = (a, b) => a % b;
 console.log(modulus(11, 3));
 
-const divide = (x, y) => x / y;
-console.log(divide(15, 2));
+const dividenumber = (x, y) => x / y;
+console.log(dividenumber(15, 2));
 
 const add = (x, y) => x + y;
 console.log(add(1022, 2323));
