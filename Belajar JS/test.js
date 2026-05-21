23053a37760b781163d64d7a35da003403fca16d
zyqri <zyqrifasya17@gmail.com>
2026-05-21 21:51:52 +0700

    Menambahkan fitur Pembagian

--- Belajar JS/test.js
+++ Belajar JS/test.js
@@ -1,5 +1,5 @@
 const modulus = (a, b) => a % b;
 console.log(modulus(11, 3));
 
-const multiply = (x, y) => x * y;
-console.log(multiply(4, 7));
+const divide = (x, y) => x / y;
+console.log(divide(15, 2));
